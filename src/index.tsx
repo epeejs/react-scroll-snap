a2f86541dbd88d9934335ca5e8cdf940e91e4821
zhangjw05 <zhangjw05@mingyuanyun.com>
2021-11-30 16:35:11 +0800

    feat: 支持 lodash 按需加载

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import { debounce } from 'lodash';
+import debounce from 'lodash/debounce';
 import React, { useEffect, useMemo, useRef } from 'react';
 import './index.less';
 
